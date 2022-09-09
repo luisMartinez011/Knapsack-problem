b2d884160e7c18a66fe7404f7f252b721081b810
Your Name <luis.martinez33@protonmail.com>
2022-09-09 16:16:32 -0500

    problema mejorado

--- knapsack.r
+++ knapsack.r
@@ -1,71 +1,86 @@
-start.time <- Sys.time()
+n<- readline(prompt="Enter the number of items: ")
+v_min <- readline(prompt="Enter the min volume: ")
+v_max <- readline(prompt="Enter the max volume: ")
+w_min <- readline(prompt="Enter the min weight: ")
+w_max <- readline(prompt="Enter the max weight: ")
 
-opcion <- 2
+n<- as.numeric(n)
+v_min<- as.numeric(v_min)
+v_max<- as.numeric(v_max)
+w_min<- as.numeric(w_min)
+w_max<- as.numeric(w_max)
 
-n = 1000000
-v_min <- 1
-v_max <- 10
-w_min <- 1
-w_max <- 25
+exitLoop <- 0
+
 
 W <- (0.3)*n*((w_min + w_max) / 2)
-#items <- sample(1:n, n, replace=TRUE)
-cat("peso total", W) 
+ 
 items <- c(1:n)
 
 
 value=sample(v_min:v_max, n, replace=TRUE)
 weight=sample(w_min:w_max, n, replace=TRUE)
 data=data.frame(items,weight,value)
-cat("\nnumero total de items", n) 
-cat("\n")
+
+rig <- readline(prompt="Enter the name for the instance file: ")
+rig <- paste(rig, ".txt")
+write.table(data, file = rig, sep = "\t",
+            row.names = FALSE)
+
 #data
 
-#create the function that we want to optimize
+
 byValue=function()
 {
+    cat("\n Heuristic by value\n")
     current_weight <- 0 
     current_value <- 0 
     sortedByValue <- data[order(-value),]
-    #print(sortedByValue)
     
     for(i in 1:nrow(sortedByValue)){
-        #print(sortedByValue$value[i])
+        
         if(current_weight + sortedByValue$weight[i] > W){
-            break;
+            
+            if(i == nrow(sortedByValue)){
+                break;
+            }else{
+                next;
+            }  
         }
         current_weight <- sortedByValue$weight[i] + current_weight
         current_value <- sortedByValue$value[i] + current_value
     }
-    cat("\n Peso final: ", current_weight)
-    cat("\n Valor final: ", current_value)
+    cat("\n Final weight: ", current_weight)
+    cat("\n Final value: ", current_value)
+    cat("\n ********************\n")
 }
 
 
 byWeight=function()
 {
+    cat("\n ********************\n")   
+    cat("\n Heuristic by weight\n")
     current_weight <- 0 
     current_value <- 0 
     sortedByWeight <- data[order(weight),]
-    #print(sortedByWeight)
     
     for(i in 1:nrow(sortedByWeight)){
-        #print(sortedByWeight$weight[i])
         if(current_weight + sortedByWeight$weight[i] > W){
             break;
         }
         current_weight <- sortedByWeight$weight[i] + current_weight
         current_value <- sortedByWeight$value[i] + current_value
     }
-    cat("\n Peso final: ", current_weight)
-    cat("\n Valor final: ", current_value)
+    cat("\n Final weight: ", current_weight)
+    cat("\n Final value: ", current_value)
+    cat("\n ********************\n")
 }
 
 
 byRatio=function()
 {
-    
-    #print(data[,3]/data[,2])
+    cat("\n ********************\n")
+    cat("\n Heuristic by ratio\n")
     ratio <- data[,3]/data[,2]
     ratioDataFrame <- data.frame(data, ratio)
     sortedByratio <- ratioDataFrame[order(-ratio),]
@@ -74,7 +89,6 @@ byRatio=function()
     current_value <- 0 
     
     for(i in 1:n){
-        #print(sortedByratio$weight[i])
         if(current_weight + sortedByratio$weight[i] > W){
             break;
         }
@@ -82,20 +96,57 @@ byRatio=function()
         current_value <- sortedByratio$value[i] + current_value
     }
     
-    cat("\n Peso final: ", current_weight)
-    cat("\n Valor final: ", current_value)
+    cat("\n Final weight: ", current_weight)
+    cat("\n Final value: ", current_value)
+    cat("\n ********************\n")
 }
 
-if(opcion==1){
-    byValue()
-} else if( opcion == 2){
-    byWeight()
-}else{
-    byRatio()
+
+
+while(exitLoop <1){
+    cat("\nSelect an heuristic:")
+    cat("\n1. Heuristic 1")
+    cat("\n2. Heuristic 2")
+    cat("\n3. Heuristic 3")
+    cat("\n4. Run all the Heuristics")
+    cat("\n5. Exit")
+    optionHeuristic <- readline("\n")
+
+
+
+
+    if(optionHeuristic == 1){
+        start.time <- Sys.time()
+        byValue()
+    }else if(optionHeuristic == 2){
+        start.time <- Sys.time()
+        byWeight()
+        end.time <- Sys.time()
+time.taken <- end.time - start.time
+
+    }else if(optionHeuristic == 3){
+        start.time <- Sys.time()
+        byRatio()
+        end.time <- Sys.time()
+time.taken <- end.time - start.time
+    }else if(optionHeuristic == 4){
+        start.time <- Sys.time()
+        byValue()
+        byWeight()
+        byRatio()
+        end.time <- Sys.time()
+    time.taken <- end.time - start.time
+    }else{
+        exitLoop = 1
+    }
+    
+    cat("\n")
+cat("Knapsack capacity: ", W)
+cat("\nItems: ", n) 
+cat("\nTime taken: ",time.taken)
+cat("\n")
+cat("\n ********************\n")
 }
 
 
 cat("\n")
-end.time <- Sys.time()
-time.taken <- end.time - start.time
-time.taken
--- tempCodeRunnerFile.r
+++
@@ -1,6 +0,0 @@
-item=c('raincoat','pocket knife','mineral water','gloves','sleeping bag','tent','portable stove','canned food','snacks')
-weight=c(2,1,6,1,4,9,5,8,3)
-survival=c(5,3,15,5,6,18,8,20,8)
-data=data.frame(item,weight,survival)
-max_weight=25
-max_weight
